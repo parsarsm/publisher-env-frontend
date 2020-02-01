e9414580be3e5fe26d28f247e99a85dfb6f85bcf
parsarsm <rostamiparsa77@gmail.com>
2020-02-01 13:21:47 +0330

    POSTS - partially done!

--- src/pagesV2/homePage/homePage.js
+++ src/pagesV2/homePage/homePage.js
@@ -1,10 +1,31 @@
 import React from 'react'
 import {Button, Comment, Container, Feed, Form, Grid, Header, Icon, Image, Input, Menu, Modal} from "semantic-ui-react";
 import SearchBar from "../../componentsV2/search/Search";
+import Posts from "../../componentsV2/posts/Posts";
 
 
 export default class HomePage extends React.Component {
-    state = {activeItem: 'home'}
+    state = {
+        activeItem: 'home',
+        posts: [
+            {
+                id: 1,
+                title: 'Post Title',
+                userId: 1,
+                userName: 'Elliot Fu',
+                userAvatar: 'https://react.semantic-ui.com/images/avatar/small/elliot.jpg',
+                timeElapsed: '2 Hours',
+                likeNum: 4,
+                dislikeNum: 2,
+                commentNum: 6,
+                comments: [],
+                // images: ['https://react.semantic-ui.com/images/wireframe/image.png','https://react.semantic-ui.com/images/wireframe/image.png'],
+                text: 'Ours is a life of constant reruns. We\'re always circling back to where we\'d we started, then starting all over again. Even if we don\'t run extra laps that day, we surely will come back for more of the same another day soon.',
+                fullText:  // with images!
+                    'Ours is a life of constant reruns. We\'re always circling back to where we\'d we started, then starting all over again. Even if we don\'t run extra laps that day, we surely will come back for more of the same another day soon.'
+            }
+        ]
+    };
 
     handleItemClick = (e, {name}) => this.setState({activeItem: name})
 
@@ -16,174 +37,7 @@ export default class HomePage extends React.Component {
 
                 <Grid stackable>
                     <Grid.Column width={13}>
-                        <Feed>
-                            <Feed.Event>
-                                <Feed.Label>
-                                    <img src='https://react.semantic-ui.com/images/avatar/small/elliot.jpg'/>
-                                </Feed.Label>
-                                <Feed.Content>
-                                    <Feed.Summary>
-                                        <Feed.User>Elliot Fu</Feed.User> added you as a friend
-                                        <Feed.Date>1 Hour Ago</Feed.Date>
-                                    </Feed.Summary>
-                                    <Feed.Extra images>
-                                        <a>
-                                            <img src='https://react.semantic-ui.com/images/wireframe/image.png'/>
-                                        </a>
-                                        <a>
-                                            <img src='https://react.semantic-ui.com/images/wireframe/image.png'/>
-                                        </a>
-                                    </Feed.Extra>
-                                    <Feed.Extra text>
-                                        Ours is a life of constant reruns. We're always circling back to where
-                                        we'd we started, then starting all over again. Even if we don't run
-                                        extra laps that day, we surely will come back for more of the same
-                                        another day soon.
-                                    </Feed.Extra>
-                                    <Feed.Meta>
-                                        <Feed.Like>
-                                            <Icon name='like'/>4 Likes
-                                        </Feed.Like>
-                                        <Modal trigger={<Feed.Like>
-                                            <Icon name='comments'/>4 Comments
-                                        </Feed.Like>}>
-                                            <Modal.Header>Post</Modal.Header>
-                                            <Modal.Content image scrolling>
-                                                <Image size='medium'
-                                                       src='https://react.semantic-ui.com/images/wireframe/image.png'
-                                                       wrapped/>
-
-                                                <Modal.Description>
-                                                    <Header>Post Title</Header>
-                                                    <p>
-                                                        This is an example of expanded content that will cause the
-                                                        modal's
-                                                        dimmer to scroll  This is an example of expanded content that will cause the
-                                                        modal's
-                                                        dimmer to scroll  This is an example of expanded content that will cause the
-                                                        modal's
-                                                        dimmer to scroll  This is an example of expanded content that will cause the
-                                                        modal's
-                                                        dimmer to scroll
-                                                    </p>
-
-                                                    <Comment.Group>
-                                                        <Header as='h3' dividing>
-                                                            Comments
-                                                        </Header>
-
-                                                        <Comment>
-                                                            <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/matt.jpg' />
-                                                            <Comment.Content>
-                                                                <Comment.Author as='a'>Matt</Comment.Author>
-                                                                <Comment.Metadata>
-                                                                    <div>Today at 5:42PM</div>
-                                                                </Comment.Metadata>
-                                                                <Comment.Text>How artistic!</Comment.Text>
-                                                                <Comment.Actions>
-                                                                    <Comment.Action>Reply</Comment.Action>
-                                                                </Comment.Actions>
-                                                            </Comment.Content>
-                                                        </Comment>
-
-                                                        <Comment>
-                                                            <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' />
-                                                            <Comment.Content>
-                                                                <Comment.Author as='a'>Elliot Fu</Comment.Author>
-                                                                <Comment.Metadata>
-                                                                    <div>Yesterday at 12:30AM</div>
-                                                                </Comment.Metadata>
-                                                                <Comment.Text>
-                                                                    <p>This has been very useful for my research. Thanks as well!</p>
-                                                                </Comment.Text>
-                                                                <Comment.Actions>
-                                                                    <Comment.Action>Reply</Comment.Action>
-                                                                </Comment.Actions>
-                                                            </Comment.Content>
-                                                            <Comment.Group>
-                                                                <Comment>
-                                                                    <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/jenny.jpg' />
-                                                                    <Comment.Content>
-                                                                        <Comment.Author as='a'>Jenny Hess</Comment.Author>
-                                                                        <Comment.Metadata>
-                                                                            <div>Just now</div>
-                                                                        </Comment.Metadata>
-                                                                        <Comment.Text>Elliot you are always so right :)</Comment.Text>
-                                                                        <Comment.Actions>
-                                                                            <Comment.Action>Reply</Comment.Action>
-                                                                        </Comment.Actions>
-                                                                    </Comment.Content>
-                                                                </Comment>
-                                                            </Comment.Group>
-                                                        </Comment>
-
-                                                        <Comment>
-                                                            <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/joe.jpg' />
-                                                            <Comment.Content>
-                                                                <Comment.Author as='a'>Joe Henderson</Comment.Author>
-                                                                <Comment.Metadata>
-                                                                    <div>5 days ago</div>
-                                                                </Comment.Metadata>
-                                                                <Comment.Text>Dude, this is awesome. Thanks so much</Comment.Text>
-                                                                <Comment.Actions>
-                                                                    <Comment.Action>Reply</Comment.Action>
-                                                                </Comment.Actions>
-                                                            </Comment.Content>
-                                                        </Comment>
-
-                                                        <Form reply>
-                                                            <Form.TextArea />
-                                                            <Button content='Add Reply' labelPosition='left' icon='edit' primary />
-                                                        </Form>
-                                                    </Comment.Group>
-                                                </Modal.Description>
-                                            </Modal.Content>
-                                            <Modal.Actions>
-                                                <Button primary>
-                                                    Proceed <Icon name='chevron right'/>
-                                                </Button>
-                                            </Modal.Actions>
-                                        </Modal>
-
-
-                                    </Feed.Meta>
-                                </Feed.Content>
-                            </Feed.Event>
-                            <Feed.Event>
-                                <Feed.Label>
-                                    <img src='https://react.semantic-ui.com/images/avatar/small/elliot.jpg'/>
-                                </Feed.Label>
-                                <Feed.Content>
-                                    <Feed.Summary>
-                                        <Feed.User>Elliot Fu</Feed.User> added you as a friend
-                                        <Feed.Date>1 Hour Ago</Feed.Date>
-                                    </Feed.Summary>
-                                    <Feed.Extra images>
-                                        <a>
-                                            <img src='https://react.semantic-ui.com/images/wireframe/image.png'/>
-                                        </a>
-                                        <a>
-                                            <img src='https://react.semantic-ui.com/images/wireframe/image.png'/>
-                                        </a>
-                                    </Feed.Extra>
-                                    <Feed.Extra text>
-                                        Ours is a life of constant reruns. We're always circling back to where
-                                        we'd we started, then starting all over again. Even if we don't run
-                                        extra laps that day, we surely will come back for more of the same
-                                        another day soon.
-                                    </Feed.Extra>
-                                    <Feed.Meta>
-                                        <Feed.Like>
-                                            <Icon name='like'/>4 Likes
-                                        </Feed.Like>
-                                        <Feed.Like>
-                                            <Icon name='comments'/>4 Comments
-                                        </Feed.Like>
-
-                                    </Feed.Meta>
-                                </Feed.Content>
-                            </Feed.Event>
-                        </Feed>
+                        <Posts posts={this.state.posts}/>
                     </Grid.Column>
 
                     <Grid.Column width={3}>
