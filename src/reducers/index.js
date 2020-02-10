09d9a760ab828106698e417f66f40d2a913c0dcc
Payam <payam.int@gmail.com>
2020-02-10 11:45:14 +0330

    fix

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,6 +5,7 @@ import profileReducer from "./profileReducer";
 import resetPassReducer from "./resetPassReducer";
 import latestPostsReducer from "./latestPostsReducer";
 import postsReducer from "./postsReducer";
+import searchReducer from "./searchReducer";
 
 export default (history) => combineReducers({
     router: connectRouter(history),
@@ -13,4 +14,5 @@ export default (history) => combineReducers({
     resetPass: resetPassReducer,
     latestPosts: latestPostsReducer,
     posts: postsReducer,
+    search: searchReducer,
 });
